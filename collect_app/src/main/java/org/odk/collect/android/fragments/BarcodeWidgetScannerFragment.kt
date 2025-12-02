ebec22b8a614a51316583e316d30753ed3eca4c1
macbook <macbook@MacBook-Air-de-macbook.local>
2025-12-02 15:48:03 +0000

    first commit

---
+++ collect_app/src/main/java/org/odk/collect/android/fragments/BarcodeWidgetScannerFragment.kt
@@ -0,0 +1,15 @@
+package org.odk.collect.android.fragments
+
+import org.odk.collect.externalapp.ExternalAppUtils.returnSingleValue
+
+class BarcodeWidgetScannerFragment : BarCodeScannerFragment() {
+    override fun isQrOnly(): Boolean {
+        return false
+    }
+
+    override fun handleScanningResult(result: String) {
+        if (isAdded) {
+            returnSingleValue(requireActivity(), result)
+        }
+    }
+}
